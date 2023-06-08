f73a26fd44474b627e0c92c1985f9c8fb081437d
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-06-08 10:33:28 +0700

    Added exmaple dock menu

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const {globalShortcut} = require('electron')
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
+
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1920,
-        height: 1080,
+        height: 1080
     })
 
     win.loadURL('http://localhost:3000/')
 }
+
+
+const dockMenu = Menu.buildFromTemplate([
+    {
+        label: 'New Window',
+        click() {
+            console.log('New Window')
+        }
+    }, {
+        label: 'New Window with Settings',
+        submenu: [
+            {label: 'Basic'},
+            {label: 'Pro'}
+        ]
+    },
+    {label: 'New Command...'}
+])
 
 app.whenReady().then(() => {
     // quit shortcut
@@ -16,5 +34,8 @@ app.whenReady().then(() => {
         app.quit();
     });
 
-
+    // dock menu
+    if (process.platform === 'darwin') {
+        app.dock.setMenu(dockMenu)
+    }
 }).then(createWindow)
