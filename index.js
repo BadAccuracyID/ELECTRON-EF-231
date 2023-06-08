afd75e42548bcd76523ec53a3c094e0d4a980e92
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-06-08 14:37:06 +0700

    Added more keyboard shortcuts and added about app page

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const {globalShortcut} = require('electron')
-const {app, BrowserWindow, Menu} = require('electron')
+const {app, BrowserWindow, Menu, MenuItem} = require('electron')
 
+
+let win = null
 
 const createWindow = () => {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 1920,
         height: 1080
     })
@@ -11,29 +12,70 @@ const createWindow = () => {
     win.loadURL('http://localhost:3000/')
 }
 
-
 const dockMenu = Menu.buildFromTemplate([
     {
-        label: 'New Window',
+        label: 'Log out',
         click() {
-            console.log('New Window')
+            // go to login page
+            win.loadURL('http://localhost:3000/auth/logout')
         }
-    }, {
-        label: 'New Window with Settings',
-        submenu: [
-            {label: 'Basic'},
-            {label: 'Pro'}
-        ]
-    },
-    {label: 'New Command...'}
+    }
 ])
 
-app.whenReady().then(() => {
-    // quit shortcut
-    globalShortcut.register('CommandOrControl+W', () => {
-        app.quit();
+function createAboutWindow() {
+    const aboutWindow = new BrowserWindow({
+        width: 450,
+        height: 450,
+        resizable: false,
+        title: 'About',
+        // Set parent window to the current focused window
+        parent: BrowserWindow.getFocusedWindow()
     });
 
+    // Load the HTML file for the about window
+    aboutWindow.loadFile('./about.html');
+}
+
+const menu = new Menu()
+menu.append(new MenuItem({
+    label: 'SiLVoam Hospital',
+    submenu: [
+        {
+            label: 'About',
+            click: () => {
+                createAboutWindow()
+            }
+        },
+        {
+            label: 'Reload',
+            role: 'Reload',
+            accelerator: process.platform === 'darwin' ? 'Cmd+R' : 'Ctrl+R',
+            click: () => {
+                // Refresh web contents
+                BrowserWindow.getFocusedWindow().webContents.reload()
+            }
+        },
+        {
+            label: 'Quit SiLVoam Hospital',
+            role: 'Quit',
+            accelerator: process.platform === 'darwin' ? 'Cmd+Q' : 'Ctrl+Q',
+            click: () => {
+                app.quit()
+            }
+        },
+        {
+            label: 'Log out',
+            accelerator: process.platform === 'darwin' ? 'Cmd+L' : 'Ctrl+L',
+            click() {
+                // go to login page
+                win.loadURL('http://localhost:3000/auth/logout')
+            }
+        }
+    ]
+}))
+Menu.setApplicationMenu(menu)
+
+app.whenReady().then(() => {
     // dock menu
     if (process.platform === 'darwin') {
         app.dock.setMenu(dockMenu)
